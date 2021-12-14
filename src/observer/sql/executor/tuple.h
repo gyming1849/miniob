2e205cee9b71d29fe6a8e7eb60a116dd74419886
gyming <gyming1849@gmail.com>
2021-12-14 21:13:08 +0800

    select

--- src/observer/sql/executor/tuple.h
+++ src/observer/sql/executor/tuple.h
@@ -89,7 +89,7 @@ public:
   TupleSchema() = default;
   ~TupleSchema() = default;
 
-  void add(AttrType type, const char *table_name, const char *field_name);
+  void add(AttrType type, const char *table_name, const char *field_name, AggregationFunc aggregation_type);
   void add_if_not_exists(AttrType type, const char *table_name, const char *field_name);
   // void merge(const TupleSchema &other);
   void append(const TupleSchema &other);
